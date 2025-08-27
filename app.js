d31b0ce496533c883ceb5c0258a37a3133794109
Daniel Yoman <danielyoma17@gmail.com>
2025-08-27 20:34:16 +0000

    create random

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log('hello')
+
+const randomNumber =  document.querySelector('#randomNum')
+
+randomNumber.addEventListener('click',()=>{
+    document.querySelector('#result').textContent = Math.random()
+})
