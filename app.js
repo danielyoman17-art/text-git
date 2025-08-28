9b8d44e3752952a94d4c6e6fb606942d76f998be
Daniel Yoman <danielyoma17@gmail.com>
2025-08-28 13:46:59 +0000

    update min max in js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const randomNumber =  document.querySelector('#randomNum')
+const min =  document.querySelector('#min')
+const max =  document.querySelector('#max')
+
+
 
 randomNumber.addEventListener('click',()=>{
-    document.querySelector('#result').textContent = Math.floor(getRandomNumber(1,100))
+    document.querySelector('#result').textContent = Math.floor(getRandomNumber(Number(min.value),Number(max.value)+1))
 })
 
 function getRandomNumber(min,max) {
