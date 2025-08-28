0e36e24746ed5f56aba58f53aa7c429e25624fcf
Daniel Yoman <danielyoma17@gmail.com>
2025-08-28 10:26:51 +0000

    add get random number

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ console.log('hello')
 const randomNumber =  document.querySelector('#randomNum')
 
 randomNumber.addEventListener('click',()=>{
-    document.querySelector('#result').textContent = Math.random()
+    document.querySelector('#result').textContent = Math.floor(getRandomNumber(1,100))
 })
+
+function getRandomNumber(min,max) {
+    return Math.random() * (max - min) + min
+}
